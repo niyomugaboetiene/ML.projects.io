53b6e32f208cdb77d70b638d76f034a2260c6b4a
nietiene <niyomugaboetiene53@gmail.com>
2025-08-31 09:30:34 -0700

    commit of image classification in machine learning

--- image-classificaation.py
+++ image-classificaation.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-# suppose we have 3 classes
+# suppose we have 3 classes this is cateogies our model will predict
 classes = ["Cat", "Dog", "Rabbit"]
 
 # toy image just numbers not real image 
 image = np.array([[1, 0],
                   [0, 1]]
                   )
-# predict our model outputs
+# predict our model outputs this define how each model is important
 logits = np.array([2.0, 5.0, 6.0])
 
-# apply numercal stable softmax
+# apply numercal stable softmax to avoid crash of the program
 shifted_logits = logits - np.max(logits)
 
 # exponentiate
@@ -20,6 +20,8 @@ probs = expoential / np.sum(expoential)
 print("Probabilities", probs)
 
 # pick predicted class
+# np.argmax return the index of maximum probability
 prexicted_index = np.argmax(probs)
+# convert the index into human readable like index 2 -> rabits
 predicted_class = classes[prexicted_index]
 print("Predicted class", predicted_class)
