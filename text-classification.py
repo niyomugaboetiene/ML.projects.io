21667f875fefb2c8d7462071ffbfc097dad219b9
nietiene <niyomugaboetiene53@gmail.com>
2025-08-31 09:49:21 -0700

    commit of image classification in machine learning

---
+++ text-classification.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+text = ["Postitive", "Neural", "Negative"]
+logits = np.array([2.0, 0.5, -1.4])
+shifted_logits = logits - np.max(logits)
+exponential = np.exp(shifted_logits)
+probs = exponential / np.sum(exponential)
+print("Probabilities", probs)
+
+predicted_index = np.argmax(probs)
+predicted_class = classes[predicted_index]
+
+print("Predicted text", predicted_class)
