f0b05c0650515ed42fd31860b98faca7ee3460ad
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-02 15:43:24 +0200

    [update] bengaluru real estate price prediction. start building the flask server

--- Data Science Project/Real Estate Price prediction/server/server.py
+++ Data Science Project/Real Estate Price prediction/server/server.py
@@ -3,10 +3,20 @@ import util
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def say_hello():
-    return "Hi"
+# @app.route("/hello")
+# def say_hello():
+#     return "Hi"
 
+
+@app.route("/get_location_names")
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+
+    response.headers.add("Access-Control-Allow-Origin", "*")
+
+    return response
 
 if __name__ == "__main__":
     print("Flask server started successfully")
