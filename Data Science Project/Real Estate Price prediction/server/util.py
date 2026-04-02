8f2ae0bea71008c9c1fd5a1c27b9ac943113b6a0
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-02 15:48:11 +0200

    [update] bengaluru real estate price prediction. start building the flask server

---
+++ Data Science Project/Real Estate Price prediction/server/util.py
@@ -0,0 +1,13 @@
+
+__location = None
+__data_columns = None
+__model = None
+
+def get_location_names():
+   pass
+
+
+def load_saved_artifacts():
+    
+if __name__ == "__main__":
+    print(get_location_names())
