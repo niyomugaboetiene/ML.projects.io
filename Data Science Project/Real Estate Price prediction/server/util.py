cac99fef2bfe58820c280a6ac69b14446734490a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-02 15:50:07 +0200

    [update] bengaluru real estate price prediction. start building the flask server

--- Data Science Project/Real Estate Price prediction/server/util.py
+++ Data Science Project/Real Estate Price prediction/server/util.py
@@ -1,3 +1,4 @@
+import json
 
 __location = None
 __data_columns = None
@@ -9,6 +10,9 @@ def get_location_names():
 
 def load_saved_artifacts():
     print("Loading saved artifacts")
-    
+
+    with open("./artifacts/columns.json", "r") as f:
+        json.load(f)
+        
 if __name__ == "__main__":
     print(get_location_names())
