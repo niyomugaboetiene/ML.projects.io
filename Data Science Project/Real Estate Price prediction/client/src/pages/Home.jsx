4b8bef4937f819b436c458a91dfba1c148d18716
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-03 18:55:53 +0200

    [Coding] building client side

--- Data Science Project/Real Estate Price prediction/client/src/pages/Home.jsx
+++ Data Science Project/Real Estate Price prediction/client/src/pages/Home.jsx
@@ -5,9 +5,9 @@ const HomeComponent = () => {
 
 
     return (
-        <div className="bg-stone-950 shadow-lg">
-            <div className="flex p-3 justify-between ">
-                <img src={vector} alt="Logo" className="w-25 "/>
+        <div className="">
+            <div className="bg-stone-950  flex p-3 justify-between shadow-lg">
+                <img src={vector} alt="Logo" className="w-25 relative left-4 hover:scale-150 transition duration-200" title="Logo"/>
                 <nav className="justify-between space-x-24 flex text-stone-400 font-serif items-center text-[18px]">
                    <Link className="hover:text-stone-500 transition-colors">Home</Link>
                    <Link className="hover:text-stone-500 transition-colors">Service</Link>
@@ -16,9 +16,17 @@ const HomeComponent = () => {
                 </nav>
 
              <div>
-                <button className="text-stone-400">Sign Up</button>
+                <button className="text-stone-400 relative right-5 top-7 bg-stone-600 px-6 py-2 rounded-lg hover:bg-stone-700 transition-colors">Sign Up</button>
 
              </div>
+            </div>
+
+            <div>
+                <div>
+                    <h1>Predict Best Price Of Dream House</h1>
+
+                  <p>Explore our smart house price prediction system that helps you estimate property values based on location, size, and features. Make better decisions with data-driven insights.</p>
+                </div>
             </div>
         </div>
     )
