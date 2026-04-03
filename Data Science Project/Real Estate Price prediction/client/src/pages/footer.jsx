c48488a6d876aa5d9e75c6b9dce81f56a8f4d817
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-03 20:14:15 +0200

    [Coding] building client side

--- Data Science Project/Real Estate Price prediction/client/src/pages/footer.jsx
+++ Data Science Project/Real Estate Price prediction/client/src/pages/footer.jsx
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div>
-             <div className="flex justify-between p-5">
+        <div className="bg-linear-to-br from-orange-400 via-red-500 to-purple-600">
+             <div className="w-[1700px] flex ms-20 rounded-xl bg-white/20 backdrop-blur-md shadow-lg">
                <div>
                 <img src={Logo} alt="Logo" />
-                <p>Our ML app help you for descion making, one click at time</p>
+                <p className="text-white">Our AI app help you for descion making, one click at time</p>
                </div>
 
-               <div>
+              <div className="flex space-x-12 mt-25">
+               <div className="flex flex-col space-x-3 text-white">
                    <span>About</span>
                    <Link>Our Story</Link>
                    <Link>Carrers</Link>
@@ -18,7 +19,7 @@ const Footer = () => {
                    <Link>Resources</Link>
                </div>
 
-               <div>
+               <div className="flex flex-col text-white">
                     <span>Support</span>
                     <Link>FAQ</Link>
                     <Link>Contact Us</Link>
@@ -26,20 +27,21 @@ const Footer = () => {
                     <Link>Terms of Service</Link>
                </div>
 
-               <div>
+               <div className="flex flex-col text-white">
                      <span>Find Us</span>
                      <Link>Events</Link>
                      <Link>Locations</Link>
                      <Link>Newsletter</Link>
                </div>
 
-               <div>
+               <div className="flex flex-col text-white">
                     <span>Our Social</span>
                     <Link>Instagram</Link>
                     <Link>Facebook</Link>
                     <Link>Twitter</Link>
                </div>
              </div>
+          </div>
         </div>
     )
 }
