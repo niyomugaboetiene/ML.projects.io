02cea5e53565288531c6f7f574ee4a8fb52156c7
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-03 20:29:24 +0200

    [Coding] building client side

--- Data Science Project/Real Estate Price prediction/client/src/pages/footer.jsx
+++ Data Science Project/Real Estate Price prediction/client/src/pages/footer.jsx
@@ -3,28 +3,28 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div className="bg-linear-to-br from-orange-400 via-red-500 to-purple-600">
-             <div className="w-[1700px] flex ms-20 rounded-xl bg-white/20 backdrop-blur-md shadow-lg">
-               <div>
-                <img src={Logo} alt="Logo" />
-                <p className="text-white">Our AI app help you for descion making, one click at time</p>
+        <div className="bg-linear-to-br from-stone-950 via-amber-700 to-stone-900">
+             <div className="w-425 flex ms-20 rounded-xl bg-white/20 backdrop-blur-md shadow-lg">
+               <div className="w-75 p-4">
+                <img src={Logo} className="w-100" alt="Logo" />
+                <p className="text-white text-lg text-center -mt-3 ">Our AI app help you for descion making, one click at time</p>
                </div>
 
-              <div className="flex space-x-12 mt-25">
+              <div className="flex space-x-30 mt-25 ms-50">
                <div className="flex flex-col space-x-3 text-white">
-                   <span>About</span>
-                   <Link>Our Story</Link>
-                   <Link>Carrers</Link>
-                   <Link>Our Team</Link>
-                   <Link>Resources</Link>
+                   <span className="text-xl font-bold mb-3">About</span>
+                   <Link className="hover:underline hover:text-md transition duration-500">Our Story</Link>
+                   <Link className="hover:underline hover:text-md transition duration-500">Carrers</Link>
+                   <Link className="hover:underline hover:text-md transition duration-500">Our Team</Link>
+                   <Link className="hover:underline hover:text-md transition duration-500">Resources</Link>
                </div>
 
                <div className="flex flex-col text-white">
-                    <span>Support</span>
-                    <Link>FAQ</Link>
-                    <Link>Contact Us</Link>
-                    <Link>Help Center</Link>
-                    <Link>Terms of Service</Link>
+                    <span className="text-xl font-bold mb-3">Support</span>
+                    <Link className="hover:underline hover:text-md transition duration-500">FAQ</Link>
+                    <Link className="hover:underline hover:text-md transition duration-500">Contact Us</Link>
+                    <Link className="hover:underline hover:text-md transition duration-500">Help Center</Link>
+                    <Link className="hover:underline hover:text-md transition duration-500">Terms of Service</Link>
                </div>
 
                <div className="flex flex-col text-white">
