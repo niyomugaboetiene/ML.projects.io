78c52283bc98bd7e9cac6bf7f617ab6f3d280e8e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-03 19:25:46 +0200

    [Coding] building client side

--- Data Science Project/Real Estate Price prediction/client/src/pages/Home.jsx
+++ Data Science Project/Real Estate Price prediction/client/src/pages/Home.jsx
@@ -7,7 +7,7 @@ const HomeComponent = () => {
 
     return (
         <div className="bg-stone-950 h-screen">
-            <div className="bg-stone-950  flex p-3 justify-between shadow-lg  border-b border-orange-600">
+            <div className="fixed top-0 right-0 left-0 bg-stone-950  flex p-3 justify-between shadow-lg  border-b border-orange-600">
                 <img src={vector} alt="Logo" className="w-25 relative left-4 hover:scale-150 transition duration-200" title="Logo"/>
                 <nav className="justify-between space-x-24 flex text-amber-700 font-serif items-center text-[18px]">
                    <Link className="hover:text-amber-800 transition-colors">Home</Link>
@@ -24,12 +24,12 @@ const HomeComponent = () => {
 
             <div className="flex  mt-12">
                 <div className="w-130 p-4 relative">
-                    <h1 className="text-[30px] absolute top-40 left-20 font-bold text-orange-500 font-serif">Predict Best Price Of Dream House</h1>
+                    <h1 className="text-[30px] absolute top-70 left-20 font-bold text-orange-500 font-serif">Predict Best Price Of Dream House</h1>
 
-                  <p className="text-[20px] absolute top-65 left-20">Explore our smart house price prediction system that helps you estimate property values based on location, size, and features. Make better decisions with data-driven insights.</p>
+                  <p className="text-[20px] absolute top-95 left-20 text-orange-400 font-serif">Explore our smart house price prediction system that helps you estimate property values based on location, size, and features. Make better decisions with data-driven insights.</p>
                 </div>
 
-                <img src={house} alt="House" title="House" className="w-250 rounded-full relative -right-90"/>
+                <img src={house} alt="House" title="House" className="w-250 rounded-full relative -right-90 top-30"/>
             </div>
         </div>
     )
