9f32d0503664fe9d581707e994a7b2eba4c96adb
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-03 19:49:26 +0200

    [Coding] building client side

--- Data Science Project/Real Estate Price prediction/client/src/pages/footer.jsx
+++ Data Science Project/Real Estate Price prediction/client/src/pages/footer.jsx
@@ -19,25 +19,25 @@ const Footer = () => {
                </div>
 
                <div>
-                <span>Support</span>
-                <Link>FAQ</Link>
-                <Link>Contact Us</Link>
-                <Link>Help Center</Link>
-                <Link>Terms of Service</Link>
+                    <span>Support</span>
+                    <Link>FAQ</Link>
+                    <Link>Contact Us</Link>
+                    <Link>Help Center</Link>
+                    <Link>Terms of Service</Link>
                </div>
 
                <div>
-                <span>Find Us</span>
-                <Link>Events</Link>
-                <Link>Locations</Link>
-                <Link>Newsletter</Link>
+                     <span>Find Us</span>
+                     <Link>Events</Link>
+                     <Link>Locations</Link>
+                     <Link>Newsletter</Link>
                </div>
 
                <div>
-                <span>Our Social</span>
-                <Link>Instagram</Link>
-                <Link>Facebook</Link>
-                <Link>Twitter</Link>
+                    <span>Our Social</span>
+                    <Link>Instagram</Link>
+                    <Link>Facebook</Link>
+                    <Link>Twitter</Link>
                </div>
              </div>
         </div>
