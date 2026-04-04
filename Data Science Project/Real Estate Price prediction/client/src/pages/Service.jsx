c3779e124962f466e827ca18b5f3d540a1f821d6
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-04 15:49:45 +0200

    [coding] implementing client side of my AI app

--- Data Science Project/Real Estate Price prediction/client/src/pages/Service.jsx
+++ Data Science Project/Real Estate Price prediction/client/src/pages/Service.jsx
@@ -10,37 +10,37 @@ export default function  Service () {
                  <p className="font-serif text-lg text-amber-800">Evaluating your home price with expertice, integrity, transparent <span className="ms-25"> and unmatched personalized service</span></p>
              </div>
 
-            <div className="flex p-3 flex-row w-400 space-x-3">
-                <div className="bg-amber-700 p-3">
-                    <div className="bg-amber-500 p-3 w-1/5 text-center rounded-lg">
-                        <FontAwesomeIcon icon={faLocationDot} className="text-[40px] text-stone-950"/>
+            <div className="flex p-3 flex-row w-300 space-x-3">
+                <div className="bg-amber-700 p-3 rounded-lg">
+                    <div className="bg-amber-500 p-3 w-1/3 text-center rounded-lg ">
+                        <FontAwesomeIcon icon={faLocationDot} className="text-[40px] text-stone-950" />
                     </div>
                     <h3 className="font-serif font-bold text-stone-950 text-lg">Expert Guidance</h3>
-                    <p>Benefit from our team's seasoned expertice for a smooth home price experience</p>
+                    <p className="text-stone-950 font-serif">Benefit from our team's seasoned expertice for a smooth home price experience</p>
                 </div>
 
-                <div>
-                    <div>
-                        <FontAwesomeIcon icon={faUserPen} />
+                <div className="bg-amber-700 p-3 rounded-lg">
+                    <div className="bg-amber-500 p-3 w-1/3 text-center rounded-lg">
+                        <FontAwesomeIcon icon={faUserPen} className="text-[40px] text-stone-950"/>
                     </div>
-                    <h3>Personalized Service</h3>
-                    <p>Our service adapt to your unique needs, making your journey stress-free</p>
+                    <h3 className="font-serif font-bold text-stone-950 text-lg">Personalized Service</h3>
+                    <p className="text-stone-950 font-serif">Our service adapt to your unique needs, making your journey stress-free</p>
                 </div>
  
-               <div>
-                <div>
-                    <FontAwesomeIcon icon={faEye}/>
+               <div className="bg-amber-700 p-3 rounded-lg">
+                <div className="bg-amber-500 p-3 w-1/3 text-center rounded-lg">
+                    <FontAwesomeIcon icon={faEye} className="text-[40px] text-stone-950" />
                 </div>
-                   <h3>Transparent Process</h3>
-                  <p>Stay informed with our clear and honest approach to buying your home</p>
+                   <h3 className="font-serif font-bold text-stone-950 text-lg">Transparent Process</h3>
+                  <p className="text-stone-950 font-serif">Stay informed with our clear and honest approach to buying your home</p>
                </div>
 
-                <div>
-                    <div>
-                       <FontAwesomeIcon icon={faHandshake}/>
+                <div className="bg-amber-700 p-3 rounded-lg">
+                    <div className="bg-amber-500 p-3 w-1/3 text-center rounded-lg">
+                       <FontAwesomeIcon icon={faHandshake} className="text-[40px] text-stone-950"/>
                     </div>
-                    <h3>Exceptional Support</h3>
-                    <p>Providing peace of mind with our responsive and attentive customer service</p>
+                    <h3 className="font-serif font-bold text-stone-950 text-lg">Exceptional Support</h3>
+                    <p className="text-stone-950 font-serif">Providing peace of mind with our responsive and attentive customer service</p>
                 </div>
             </div>
         </div>
