d42c2843e0377ba0b952efe51900a43182d2d27c
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-04 14:53:22 +0200

    [coding] implementing client side of my AI app

---
+++ Data Science Project/Real Estate Price prediction/client/src/pages/Service.jsx
@@ -0,0 +1,16 @@
+
+export default function  Service () {
+     return (
+        <div>
+            <div>
+                 <div>
+                    <h1>WHy Choose Us</h1>
+                    <p>Evaluating your home price with expertice, integrity, transparent and unmatched personalized service</p>
+                 </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+     )
+}
