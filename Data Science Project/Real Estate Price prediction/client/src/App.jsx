e4681e71ef58f63cdb0330b6a6f8e63d27743fa4
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-04-04 15:13:29 +0200

    [coding] implementing client side of my AI app

--- Data Science Project/Real Estate Price prediction/client/src/App.jsx
+++ Data Science Project/Real Estate Price prediction/client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import IndexComponent from './pages/index';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/layout';
+import Service from './pages/Service';
 import HomeComponent from './pages/Home';
 
 function App() {
@@ -17,6 +18,11 @@ function App() {
                <IndexComponent />
            </section>
        </Layout>
+
+      <Routes>
+               <Route path='/service' element={ <Service />} />
+
+      </Routes>
     </BrowserRouter>
 
   )
--- Data Science Project/Real Estate Price prediction/client/src/pages/Service.jsx
+++ Data Science Project/Real Estate Price prediction/client/src/pages/Service.jsx
@@ -1,14 +1,33 @@
+import 
 
 export default function  Service () {
      return (
-        <div>
-            <div>
-                 <div>
-                    <h1>WHy Choose Us</h1>
-                    <p>Evaluating your home price with expertice, integrity, transparent and unmatched personalized service</p>
-                 </div>
-                <div>
+        <div className="min-h-screen bg-stone-950 text-white p-2">
+             <div>
+                <h1>WHy Choose Us</h1>
+                 <p>Evaluating your home price with expertice, integrity, transparent and unmatched personalized service</p>
+             </div>
 
+            <div>
+                
+                <div>
+                    <h3>Expert Guidance</h3>
+                    <p>Benefit from our team's seasoned expertice for a smooth home price experience</p>
+                </div>
+
+                <div>
+                    <h3>Personalized Service</h3>
+                    <p>Our service adapt to your unique needs, making your journey stress-free</p>
+                </div>
+ 
+               <div>
+                   <h3>Transparent Process</h3>
+                  <p>Stay informed with our clear and honest approach to buying your home</p>
+               </div>
+
+                <div>
+                    <h3>Exceptional Support</h3>
+                    <p>Providing peace of mind with our responsive and attentive customer service</p>
                 </div>
             </div>
         </div>
